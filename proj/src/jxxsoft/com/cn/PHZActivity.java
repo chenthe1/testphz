607f2fc229bd80f7f7e4123fefd3c4de2ce998bf
jxxsoft <jxx_soft@163.com>
2016-03-31 16:38:53 +0800

    add

---
+++ proj/src/jxxsoft/com/cn/PHZActivity.java
@@ -0,0 +1,13 @@
+package jxxsoft.com.cn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PHZActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
