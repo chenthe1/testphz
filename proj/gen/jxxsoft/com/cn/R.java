5d5831be03901ac7e88fa0796744f5abf5fb9882
jxxsoft <jxx_soft@163.com>
2016-04-05 17:29:30 +0800

    show text

--- proj/gen/jxxsoft/com/cn/R.java
+++ proj/gen/jxxsoft/com/cn/R.java
@@ -11,8 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int table=0x7f020001;
+        public static final int box_off=0x7f020000;
+        public static final int box_on=0x7f020001;
+        public static final int btn_back_image=0x7f020002;
+        public static final int btn_yanzhenma_image=0x7f020003;
+        public static final int choice_paytype_item_bg=0x7f020004;
+        public static final int ddz_btn_back=0x7f020005;
+        public static final int ddz_edit_bg=0x7f020006;
+        public static final int dialog_edit_bg=0x7f020007;
+        public static final int dialog_image_bg=0x7f020008;
+        public static final int edit_input=0x7f020009;
+        public static final int hall_dialog_bg=0x7f02000a;
+        public static final int hall_edit_bg=0x7f02000b;
+        public static final int hall_payfail_dialog_bg=0x7f02000c;
+        public static final int hall_store_bg=0x7f02000d;
+        public static final int hall_store_btn_d=0x7f02000e;
+        public static final int ic_launcher=0x7f02000f;
+        public static final int information_pay_bg=0x7f020010;
+        public static final int label_alpha_image=0x7f020011;
+        public static final int login_top_bg=0x7f020012;
+        public static final int red_togame_btn=0x7f020013;
+        public static final int reg_tab_bg_on_left=0x7f020014;
+        public static final int table=0x7f020015;
+    }
+    public static final class id {
+        public static final int main_layout=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- proj/src/jxxsoft/com/cn/PHZActivity.java
+++ proj/src/jxxsoft/com/cn/PHZActivity.java
@@ -1,7 +1,19 @@
 package jxxsoft.com.cn;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.graphics.Paint.Style;
+import android.graphics.Point;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class PHZActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +21,48 @@ public class PHZActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        /*
+        Button btnExit = new Button(this);
+        btnExit.setTag("退出");
+        btnExit.setBackgroundResource(R.drawable.btn_back_image);
+        btnExit.setOnClickListener(new Button.OnClickListener(){
+			public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				finish();
+			}
+        });
+        */
+        
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.main_layout);
+        layout.addView(new MyView(this));
+      //  layout.addView(btnExit);
     }
+    
+	public class MyView extends View{
+
+		public MyView(Context context) {
+			super(context);
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			DisplayMetrics dm = new DisplayMetrics();
+			getWindowManager().getDefaultDisplay().getMetrics(dm);
+			Point pt = new Point();
+			pt.x = (dm.widthPixels - 550) / 2;
+			pt.y = dm.heightPixels * 2 / 5;
+
+			Paint paintText=new Paint();		//创建一个采用默认设置的画笔
+			paintText.setColor(0x11FFFFFF);		//设置画笔颜色
+			paintText.setTextAlign(Align.LEFT);	//设置文字左对齐
+			paintText.setTextSize(150);		//设置文字大小
+			paintText.setAntiAlias(true);	//使用抗锯齿功能
+		//	paintText.setFakeBoldText(true);
+			float[] pos= new float[]{pt.x,pt.y, pt.x+200,pt.y, pt.x+400,pt.y};	//定义代表文字位置的数组
+			canvas.drawPosText("跑胡子", pos, paintText);			//通过drawPosText()方法绘制文字
+			super.onDraw(canvas);
+		}
+	}
 }
